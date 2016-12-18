40ad8bcf498f9391dec865a68631f6bb6aa13578
Piotr <piotr.ziebinski97@gmail.com>
2016-12-18 17:09:09 +0100

    edit ~Piotrek

--- app/src/main/java/com/example/piotr/streetracing/MainFragment.java
+++ app/src/main/java/com/example/piotr/streetracing/MainFragment.java
@@ -3,22 +3,221 @@ package com.example.piotr.streetracing;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import com.example.piotr.streetracing.MyLocation.LocationResult;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainFragment extends Fragment {
+    private MapboxMap map;
+    private MapView mapView;
+    private FloatingActionButton floatingActionButton;
+    private LocationServices locationServices;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 124;
+
+    private String current_location_latitude, current_location_longitude;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View android = inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreate(savedInstanceState);
+        //MapboxAccountManager.start(this, getString(R.string.access_token));
+        MapboxAccountManager.start(getContext(), "pk.eyJ1IjoibXEwMDciLCJhIjoiY2l3dGZrZmxlMDBwbDJ6bWcyZnN4azYzdSJ9.ccx2gEnhH6Bm8QHz0vKMhg");
+        View android = inflater.inflate(R.layout.activity_main, container, false);
+        insertDummyContactWrapper();
+        Context context = getContext();
+        locationServices = LocationServices.getLocationServices(context);
 
-        //iv.setImageResource(R.drawable.home1);
+        MapView mapView = (MapView) getView().findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
 
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
+            }
+        });
+
+
+        floatingActionButton = (FloatingActionButton) getView().findViewById(R.id.location_toggle_fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+
+                    //========================Lokazlicazja
+                    LocationResult locationResult = new LocationResult(){
+                        @Override
+                        public void gotLocation(Location location){
+                            //Got the location!
+                            current_location_latitude = Location.convert(location.getLatitude(), Location.FORMAT_DEGREES);
+                            current_location_longitude = Location.convert(location.getLongitude(), Location.FORMAT_DEGREES);
+                            Toast.makeText(getActivity(), current_location_latitude+"  "+current_location_longitude, Toast.LENGTH_LONG).show();
+                        }
+                    };
+                    MyLocation myLocation = new MyLocation();
+
+                    myLocation.getLocation(getActivity(), locationResult);
+
+                }
+            }
+        });
         return android;
-    }}
+    }
 
+
+    private void insertDummyContactWrapper() {
+        int hasWriteContactsPermission = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showMessageOKCancel("You need to allow access to Localization",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_CODE_ASK_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+        // ===== = XDDDD === insertDummyContact();
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(getActivity())
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    //enableLocation(true);
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getActivity(), "ACCESS_FINE_LOCATION Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    private void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            // Check if user has granted location permission
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_ASK_PERMISSIONS);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            // If we have the last location of the user, we can move the camera to that position.
+            Location lastLocation = locationServices.getLastLocation();
+            if (lastLocation != null) {
+                //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+            }
+
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        // Move the map camera to where the user location is and then remove the
+                        // listener so the camera isn't constantly updating when the user location
+                        // changes. When the user disables and then enables the location again, this
+                        // listener is registered again and will adjust the camera once again.
+                        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
+                        locationServices.removeLocationListener(this);
+                    }
+                }
+            });
+            floatingActionButton.setImageResource(R.drawable.myloc);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.myloc);
+        }
+        // Enable or disable the location layer on the map
+        map.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
 
---
+++ app/src/main/java/com/example/piotr/streetracing/MyLocation.java
@@ -0,0 +1,131 @@
+package com.example.piotr.streetracing;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+
+public class MyLocation {
+    Timer timer1;
+    LocationManager lm;
+    LocationResult locationResult;
+    Context this_context;
+    boolean gps_enabled=false;
+    boolean network_enabled=false;
+
+    public boolean getLocation(Context context, LocationResult result) {
+        this_context = context;
+        locationResult = result;
+        if (lm == null)
+            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        if (!gps_enabled && !network_enabled)
+            return false;
+
+        String permission = "android.permission.ACCESS_COARSE_LOCATION";
+        int res = this_context.checkCallingOrSelfPermission(permission);
+        if (res == PackageManager.PERMISSION_GRANTED) {
+
+            if (gps_enabled)
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
+            if (network_enabled)
+                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+            timer1 = new Timer();
+            timer1.schedule(new GetLastLocation(), 20000);
+        }
+        // }
+        return true;
+    }
+
+    LocationListener locationListenerGps = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            String permission = "android.permission.ACCESS_COARSE_LOCATION";
+            int res = this_context.checkCallingOrSelfPermission(permission);
+            if (res == PackageManager.PERMISSION_GRANTED) {
+                locationResult.gotLocation(location);
+                lm.removeUpdates(this);
+                lm.removeUpdates(locationListenerNetwork);
+            }
+        }
+        public void onProviderDisabled(String provider) {}
+        public void onProviderEnabled(String provider) {}
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            locationResult.gotLocation(location);
+            String permission = "android.permission.ACCESS_COARSE_LOCATION";
+            int res = this_context.checkCallingOrSelfPermission(permission);
+            if (res == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(this);
+                lm.removeUpdates(locationListenerGps);
+            }
+        }
+        public void onProviderDisabled(String provider) {}
+        public void onProviderEnabled(String provider) {}
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
+
+    class GetLastLocation extends TimerTask {
+        @Override
+        public void run() {
+            String permission = "android.permission.ACCESS_COARSE_LOCATION";
+            int res = this_context.checkCallingOrSelfPermission(permission);
+            if (res == PackageManager.PERMISSION_GRANTED) {
+                lm.removeUpdates(locationListenerGps);
+                lm.removeUpdates(locationListenerNetwork);
+
+                Location net_loc = null, gps_loc = null;
+                if (gps_enabled)
+                    gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (network_enabled)
+                    net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                //if there are both values use the latest one
+                if (gps_loc != null && net_loc != null) {
+                    if (gps_loc.getTime() > net_loc.getTime())
+                        locationResult.gotLocation(gps_loc);
+                    else
+                        locationResult.gotLocation(net_loc);
+                    return;
+                }
+
+                if (gps_loc != null) {
+                    locationResult.gotLocation(gps_loc);
+                    return;
+                }
+                if (net_loc != null) {
+                    locationResult.gotLocation(net_loc);
+                    return;
+                }
+                locationResult.gotLocation(null);
+            }
+        }
+    }
+
+    public static abstract class LocationResult{
+        public abstract void gotLocation(Location location);
+    }
+
+
+}
